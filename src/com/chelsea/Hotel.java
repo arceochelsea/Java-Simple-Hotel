6fdd91103dbadda7aed26e23324d0b203c3882b1
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-11-05 23:07:48 -0500

    Created CLI class

--- src/com/chelsea/Hotel.java
+++ src/com/chelsea/Hotel.java
@@ -1,18 +1,15 @@
 package com.chelsea;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Hotel {
 
-    private Scanner scanner;
     private String guestName;
     private ArrayList<Integer> availableRooms;
     private ArrayList<Integer> unavailableRooms;
 
-    public Hotel(Scanner scanner, String guestName, ArrayList<Integer> availableRooms, ArrayList<Integer> unavailableRooms) {
+    public Hotel(String guestName, ArrayList<Integer> availableRooms, ArrayList<Integer> unavailableRooms) {
 
-        this.scanner = scanner;
         this.guestName = guestName;
         this.availableRooms = availableRooms;
         this.unavailableRooms = unavailableRooms;
@@ -24,22 +21,8 @@ public class Hotel {
         availableRooms.add(555);
     }
 
-    public void welcomePrompt() {
-        System.out.println("Please enter your name here!");
-        guestName = scanner.nextLine();
-        System.out.println("Thank you, " + guestName + " for choosing Motel 6, would you like to book a room with us today? Please write YES or NO");
-        String answer = scanner.nextLine().toLowerCase().trim();
-
-        if (answer.equals("yes")) {
-            System.out.println("Thank you, " + guestName + " you just booked a room with us today.");
-            checkInRoom();
-        } else {
-            System.out.println("Sorry, " + guestName + " we hope you choose to come back another time!");
-        }
-    }
-
     public void checkInRoom() {
-        System.out.println(guestName + ", you are now checked into room: " + availableRooms.get(0));
+        System.out.println("You are now checked into room: " + availableRooms.get(0));
 
         unavailableRooms.add(availableRooms.get(0));
         availableRooms.remove(0);
@@ -48,24 +31,10 @@ public class Hotel {
 
         displayAvailableRooms();
         displayUnavailableRooms();
-
-        guestStatus();
-    }
-
-    public void guestStatus() {
-        System.out.println("Hello, " + guestName + " are you ready to check out now? Write YES or NO");
-        String answer = scanner.nextLine().toLowerCase().trim();
-
-        if (answer.equals("yes")) {
-            checkOutRoom();
-        } else {
-            System.out.println("Sure. We will check on your status later.");
-            guestStatus();
-        }
     }
 
     public void checkOutRoom() {
-        System.out.println(guestName + ", you are now checking out of room: " + unavailableRooms.get(0));
+        System.out.println("You are now checking out of room: " + unavailableRooms.get(0));
 
         availableRooms.add(unavailableRooms.get(0));
         unavailableRooms.remove(0);
--- src/com/chelsea/Main.java
+++ src/com/chelsea/Main.java
@@ -1,13 +1,12 @@
 package com.chelsea;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Hotel myBooking = new Hotel(scanner,"", new ArrayList<>(), new ArrayList<>());
-        myBooking.welcomePrompt();
+        Hotel myBooking = new Hotel("", new ArrayList<>(), new ArrayList<>());
+        CLI.runProgram(myBooking);
+
     }
 }
